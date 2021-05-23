b6b60f7d2d316778aa2c057214e62cfa9d17182d
Sstark97 <aitorscinfo@gmail.com>
2021-05-23 20:54:29 +0100

    Try to solve some CORS problems 1.3

--- components/App.tsx
+++ components/App.tsx
@@ -34,8 +34,10 @@ export function App () {
     const [city, setCity] = useState('');
     const [error, setError] = useState(false);
     const [citySearch, setCitySearch] = useState('/location/44418');
-    const config : AxiosRequestConfig = {headers:{'Content-Type': 'application/json',"Access-Control-Allow-Origin" : "*"},
-        proxy: createProxyMiddleware({ target: 'https://www.metaweather.com/api/', changeOrigin: true})
+    const config : AxiosRequestConfig = {headers:{'Content-Type': 'application/json',"Access-Control-Allow-Origin" : "*"},proxy: {
+        host: 'https://www.metaweather.com/api/',
+        port: 8080
+      }
     
     };
 
